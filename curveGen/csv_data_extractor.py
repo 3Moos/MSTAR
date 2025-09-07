c3f5dc6758d63c42abb7654f24307c296f8b293d
3Moos <190341036+3Moos@users.noreply.github.com>
2025-09-06 18:24:07 -0700

    Fixed the delta readings. now takes all nessicarry values from the csv file. next step is to have it automatically find the files w/out need for manual input of file name and path.

--- curveGen/csv_data_extractor.py
+++ curveGen/csv_data_extractor.py
@@ -9,13 +9,7 @@ current_time = time.localtime()
 current_date_time = time.strftime("%Y-%m-%d %H:%M:%S", current_time)
 
 
-file = "/Users/moose/Library/Mobile Documents/com~apple~CloudDocs/MSTAR/MSTAR/curveGen/07082025 10% CaClO4 30% sat ink 18V NEW fine JSC W temp SENSOR 2 cell 7.5 NN.csv"
-
-with open(file, 'r', newline ="") as csvfile:
-    reader = csv.reader(csvfile) #treats each line as a string
-
-    #for row in reader:
-        #print(row) # Each 'row' is a list of strings representing the columns
+file = "/Users/moose/Desktop/MSTAR/curveGen/07082025 10% CaClO4 30% sat ink 18V NEW fine JSC W temp SENSOR 2 cell 7.5 NN.csv"
 
 
 df = pd.read_csv(file) 
@@ -29,16 +23,16 @@ oxygen = rf["Latest: Oxygen Gas (ppm)"]
 times = rf["Latest: Time (s)"]
 times_start = int(first_row["Latest: Time (s)"])
 times_end = int(last_row["Latest: Time (s)"])
-temp_start = int(first_row["Latest: Temperature (°C)"])
-temp_end = int(first_row["Latest: Temperature (°C)"])
-current_start = int(first_row["Latest: Current (A)"])
-current_end = int(first_row["Latest: Current (A)"])
+temp_start = float(first_row["Latest: Temperature (°C)"])
+temp_end = float(last_row["Latest: Temperature (°C)"])
+current_start = float(first_row["Latest: Current (A)"])
+current_end = float(last_row["Latest: Current (A)"])
 av_temp = temp.mean()
 stdev_temp = temp.std()
-temp_delta = int(current_end - current_start)
+temp_delta = float(current_end - current_start)
 av_current = current.mean()
 stdev_current = current.std()
-current_delta = int(temp_end - temp_start)
+current_delta = float(temp_end - temp_start)
 oxygen_production = np.trapezoid(oxygen, times)
 
 run_time = int(times_end - times_start) #seconds
