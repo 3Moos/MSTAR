eafaaa6aede967887dc07085cff4840d0df79bf8
John Sarrouf <sarroufjohn@gmail.com>
2025-09-16 13:00:54 -0700

    simplify and document code
    simplify and document code
    lots of unused functions
    code needed some documentation
    but more is needed for the voltage_to_ppm function
    
    csv data saving still needed

--- nitrogenSensor/main.py
+++ nitrogenSensor/main.py
@@ -1,3 +1,5 @@
+# TODO - add csv writing
+
 import analogio
 from board import *
 from time import sleep
@@ -8,60 +10,29 @@ import os
 import json
 from config import config
 
-#network_data = []
-
-# with open('network_config.json', 'w') as f:
-#     print(network_data)
-    #json.dump(network_data, f, indent=2)
-
+# define the pins for the respective nitrogen sensors (for reading voltage)
 pin = analogio.AnalogIn(A0)
 pin2 = analogio.AnalogIn(A1)
 
-def reverse_string(s):
-    result = ''
-    for char in s:
-        result = char + result
-    return result
-
-def add_commas(number):
-#     print("Number is ", number)
-    num_str = str(number)
-
-    decimals = num_str.split(".")
-    
-    length = len(decimals)
-    
-    add_commas = length % 3
-    
-    
-    print(decimals[1], add_commas)
-    split_string = decimals[1]
-    current_comma = 3
-    print("SPLIT IS", split_string[2])
-    data = ""
-    for count, char in enumerate(split_string):
-        data += char
-        print(data)
-        
-        if count == add_commas - 1:
-            data += ","
-            add_commas += 3
-            
-    return float(decimals[0])
-
-
-
+# NEED TO FIND WHAT EACH VAR REPRESENTS
+# will be determined once part name is found
 def voltage_to_ppm(voltage, inMin = 0, inMax = 3.3, outMin = 0, outMax = 20000):
-  return outMin + (float(voltage - inMin) / float(inMax - inMin) * (outMax
-                  - outMin))
+  return outMin + (float(voltage - inMin) / float(inMax - inMin) * (outMax - outMin))
 
+# set to run indefinitely, will likelly be changed in the future to account for the experiment needing to end 
 while True:
+
+    # gets the voltage from that pin in order to calculate the nitrogen ppm later on
     voltage = pin.value
     voltage2 = pin2.value
-    #ppm = add_commas(voltage_to_ppm(voltage))
+
+    # takes the voltages and through the formnula above, generates a ppm
     ppm = voltage_to_ppm(voltage)
     ppm2 = voltage_to_ppm(voltage2)
+
+    # btw, the f string formatting is just so it only shows up to a certain amount of decimals, in this case two
     print(f"DAC reading: {voltage}, PPM Conversion: {ppm:,.2f}")
     print(f"DAC2 Voltage: {voltage2}, PPM Conversion: {ppm2:,.2f}\n")
+
     sleep(1)
 
