fbc8cfa9804dc7d21b95eb77f6087d92a1b4b6c6
3Moos <190341036+3Moos@users.noreply.github.com>
2025-10-26 21:15:07 -0700

    fixed the terrible hard coded naming system I was using, now every column name is set as a variable. probably still inperfect but this is a large step up. working on adding resistance calculations next, pushing for version control. -Moos

--- freezing_chamber/analysis.py
+++ freezing_chamber/analysis.py
@@ -47,6 +47,40 @@ c14conc = "5pct"
 c15conc = "5pct"
 c16conc = "5pct"
 
+c1voltage = f'{c1conc} Cell 1 Voltage'
+c2voltage = f'{c2conc} Cell 2 Voltage'
+c3voltage = f'{c3conc} Cell 3 Voltage'
+c4voltage = f'{c4conc} Cell 4 Voltage'
+c5voltage = f'{c5conc} Cell 5 Voltage'
+c6voltage = f'{c6conc} Cell 6 Voltage'
+c7voltage = f'{c7conc} Cell 7 Voltage'
+c8voltage = f'{c8conc} Cell 8 Voltage'
+c9voltage = f'{c9conc} Cell 9 Voltage'
+c10voltage = f'{c10conc} Cell 10 Voltage'
+c11voltage = f'{c11conc} Cell 11 Voltage'
+c12voltage = f'{c12conc} Cell 12 Voltage'
+c13voltage = f'{c13conc} Cell 13 Voltage'
+c14voltage = f'{c14conc} Cell 14 Voltage'
+c15voltage = f'{c15conc} Cell 15 Voltage'
+c16voltage = f'{c16conc} Cell 16 Voltage'
+
+c1current = f'{c1conc} Cell 1 Current'
+c2current = f'{c2conc} Cell 2 Current'
+c3current = f'{c3conc} Cell 3 Current'
+c4current = f'{c4conc} Cell 4 Current'
+c5current = f'{c5conc} Cell 5 Current'
+c6current = f'{c6conc} Cell 6 Current'
+c7current = f'{c7conc} Cell 7 Current'
+c8current = f'{c8conc} Cell 8 Current'
+c9current = f'{c9conc} Cell 9 Current'
+c10current = f'{c10conc} Cell 10 Current'
+c11current = f'{c11conc} Cell 11 Current'
+c12current = f'{c12conc} Cell 12 Current'
+c13current = f'{c13conc} Cell 13 Current'
+c14current = f'{c14conc} Cell 14 Current'
+c15current = f'{c15conc} Cell 15 Current'
+c16current = f'{c16conc} Cell 16 Current'
+
 ts1 = f'{c2conc} Cell 2 Temp'
 ts2 = f'{c4conc} Cell 4 Temp'
 ts3 = f'{c6conc} Cell 6 Temp'
@@ -56,45 +90,64 @@ ts6 = f'{c11conc} Cell 11 Temp'
 ts7 = f'{c13conc} Cell 13 Temp'
 ts8 = f'{c15conc} Cell 15 Temp'
 
+c1temp = f'{c1conc} Cell 1 Temp'
+c2temp = ts1
+c3temp = f'{c3conc} Cell 3 Temp'
+c4temp = ts2
+c5temp = f'{c5conc} Cell 5 Temp'
+c6temp = ts3
+c7temp = f'{c7conc} Cell 7 Temp'
+c8temp = ts4
+c9temp = ts5
+c10temp = f'{c10conc} Cell 10 Temp'
+c11temp = ts6
+c12temp = f'{c12conc} Cell 12 Temp'
+c13temp = ts7
+c14temp = f'{c14conc} Cell 14 Temp'
+c15temp = ts8
+c16temp = f'{c16conc} Cell 16 Temp'
+
+
+
 
 df_p1 = df_p1.rename(columns={
     "Timestamp_p1" : "Timestamp",
-    "mA 1_p1" : f"{c1conc}Cell 1 Current",
-    "V1_p1" : f"{c1conc}Cell 1 Voltage",
-    "mA 2_p1" : f"{c1conc}Cell 2 Current",
-    "V2_p1" : f"{c2conc}Cell 2 Voltage",
-    "mA 3_p1" : f"{c3conc}Cell 3 Current",
-    "V3_p1" : f"{c3conc}Cell 3 Voltage",
-    "mA 4_p1" : f"{c4conc}Cell 4 Current",
-    "V4_p1" : f"{c4conc}Cell 4 Voltage",
-    "mA 5_p1" : f"{c5conc}Cell 5 Current",
-    "V5_p1" : f"{c5conc}Cell 5 Voltage",
-    "mA 6_p1" : f"{c6conc}Cell 6 Current",
-    "V6_p1" : f"{c6conc}Cell 6 Voltage",
-    "mA 7_p1" : f"{c7conc}Cell 7 Current",
-    "V7_p1" : f"{c7conc}Cell 7 Voltage",
-    "mA 8_p1" : f"{c8conc}Cell 8 Current",
-    "V8_p1" : f"{c8conc}Cell 8 Voltage"
+    "mA 1_p1" : c1current,
+    "V1_p1" : c1voltage,
+    "mA 2_p1" : c2current,
+    "V2_p1" : c2voltage,
+    "mA 3_p1" : c3current,
+    "V3_p1" : c3voltage,
+    "mA 4_p1" : c4current,
+    "V4_p1" : c4voltage,
+    "mA 5_p1" : c5current,
+    "V5_p1" : c5voltage,
+    "mA 6_p1" : c6current,
+    "V6_p1" : c6voltage,
+    "mA 7_p1" : c7current,
+    "V7_p1" : c7voltage,
+    "mA 8_p1" : c8current,
+    "V8_p1" : c8voltage,
 }
                      )
 
 df_p2 = df_p2.rename(columns={
-    "mA 1_p2" : f"{c9conc}Cell 9 Current",
-    "V1_p2" : f"{c9conc}Cell 9 Voltage",
-    "mA 2_p2" : f"{c10conc}Cell 10 Current",
-    "V2_p2" : f"{c10conc}Cell 10 Voltage",
-    "mA 3_p2" : f"{c11conc}Cell 11 Current",
-    "V3_p2" : f"{c11conc}Cell 11 Voltage",
-    "mA 4_p2" : f"{c12conc}Cell 12 Current",
-    "V4_p2" : f"{c12conc}Cell 12 Voltage",
-    "mA 5_p2" : f"{c13conc}Cell 13 Current",
-    "V5_p2" : f"{c13conc}Cell 13 Voltage",
-    "mA 6_p2" : f"{c14conc}Cell 14 Current",
-    "V6_p2" : f"{c14conc}Cell 14 Voltage",
-    "mA 7_p2" : f"{c15conc}Cell 15 Current",
-    "V7_p2" : f"{c15conc}Cell 15 Voltage",
-    "mA 8_p2" : f"{c16conc}Cell 16 Current",
-    "V8_p2" : f"{c16conc}Cell 16 Voltage"
+    "mA 1_p2" : c9current,
+    "V1_p2" : c9voltage,
+    "mA 2_p2" : c10current,
+    "V2_p2" : c10voltage,
+    "mA 3_p2" : c11current,
+    "V3_p2" : c11voltage,
+    "mA 4_p2" : c12current,
+    "V4_p2" : c12voltage,
+    "mA 5_p2" : c13current,
+    "V5_p2" : c13voltage,
+    "mA 6_p2" : c14current,
+    "V6_p2" : c14voltage,
+    "mA 7_p2" : c15current,
+    "V7_p2" : c15voltage,
+    "mA 8_p2" : c16current,
+    "V8_p2" : c16voltage,
 }
                      )
 
@@ -156,9 +209,7 @@ df_t2.to_csv(f"{experiment_name}_temps_averaged_by_time_{timestamp}.csv", index=
 
 
 
-
-#Vibe End
-
+#Calculating resistanc values
 
 
 #df_p1 = df_p1.apply(pd.to_numeric, errors="coerce") 
@@ -197,55 +248,54 @@ combined_data = pd.concat([power_data_p1, power_data_p2, temp_data_t1, temp_data
 
 order = [ #HARDCODED, CHANGE TO MATCH EXPERIMENT
     'Timestamp',
-    f"{c1conc}Cell 1 Current",
-    f"{c1conc}Cell 1 Voltage",
-    f"{c1conc} Cell 1 Temp",
-    f"{c2conc}Cell 2 Current",
-    f"{c2conc}Cell 2 Voltage",
-    ts1,
-    f"{c3conc}Cell 3 Current",
-    f"{c3conc}Cell 3 Voltage",
-    f"{c3conc} Cell 3 Temp",
-    f"{c4conc}Cell 4 Current",
-    f"{c4conc}Cell 4 Voltage",
-    ts2,
-    f"{c5conc}Cell 5 Current",
-    f"{c5conc}Cell 5 Voltage",
-    f"{c5conc} Cell 5 Temp",
-    f"{c6conc}Cell 6 Current",
-    f"{c6conc}Cell 6 Voltage",
-    ts3,
-    f"{c7conc}Cell 7 Current",
-    f"{c7conc}Cell 7 Voltage",
-    f"{c7conc} Cell 7 Temp",
-    f"{c8conc}Cell 8 Current",
-    f"{c8conc}Cell 8 Voltage",
-    ts4,
-    f"{c9conc}Cell 9 Current",
-    f"{c9conc}Cell 9 Voltage",
-    ts5,
-    f"{c10conc}Cell 10 Current",
-    f"{c10conc}Cell 10 Voltage",
-    f"{c10conc} Cell 10 Temp",
-    f"{c11conc}Cell 11 Current",
-    f"{c11conc}Cell 11 Voltage",
-    ts6,
-    f"{c12conc}Cell 12 Current",
-    f"{c12conc}Cell 12 Voltage",
-    f"{c12conc} Cell 12 Temp",
-    f"{c13conc}Cell 13 Current",
-    f"{c13conc}Cell 13 Voltage",
-    ts7,
-    f"{c14conc}Cell 14 Current",
-    f"{c14conc}Cell 14 Voltage",
-    f"{c14conc} Cell 14 Temp",
-    f"{c15conc}Cell 15 Current",
-    f"{c15conc}Cell 15 Voltage",
-    ts8,
-    f"{c16conc}Cell 16 Current",
-    f"{c16conc}Cell 16 Voltage",
-    f"{c16conc} Cell 16 Temp"
-
+    c1current,
+    c1voltage,
+    c1temp,
+    c2current,
+    c2voltage,
+    c2temp,
+    c3current,
+    c3voltage,
+    c3temp,
+    c4current,
+    c4voltage,
+    c4temp,
+    c5current,
+    c5voltage,
+    c5temp,
+    c6current,
+    c6voltage,
+    c6temp,
+    c7current,
+    c7voltage,
+    c7temp,
+    c8current,
+    c8voltage,
+    c8temp,
+    c9current,
+    c9voltage,
+    c9temp,
+    c10current,
+    c10voltage,
+    c10temp,
+    c11current,
+    c11voltage,
+    c11temp,
+    c12current,
+    c12voltage,
+    c12temp,
+    c13current,
+    c13voltage,
+    c13temp,
+    c14current,
+    c14voltage,
+    c14temp,
+    c15current,
+    c15voltage,
+    c15temp,
+    c16current,
+    c16voltage,
+    c16temp
 ]
 
 combined_data_ordered = combined_data[order]
@@ -254,7 +304,7 @@ combined_data_ordered.to_csv(f'{experiment_name}combined_data_{timestamp}.csv', 
 
 #combined_data.to_csv(f'combined_data.csv_{timestamp}', index=False) #index=False removes the labing index that is to the left of the data by default e.g. 1,2,3,4,5,6,7,...,n
 
-power_on_data = combined_data_ordered[(combined_data_ordered[f"{c9conc}Cell 9 Voltage"] > 5)] 
+power_on_data = combined_data_ordered[(combined_data_ordered[c9voltage] > 5)] 
 
 power_on_data.to_csv(f'{experiment_name}power_on_data_{timestamp}.csv', index=False)
 
@@ -262,50 +312,11 @@ power_on_data.to_csv(f'{experiment_name}power_on_data_{timestamp}.csv', index=Fa
 
 #Everything below this line is for analysis of the data
 
-#Okay I need to basically parse each induvidual set of columns(ignoring timestamp) and then output all the data set for the specified value of current I was looking for then repeat to all of the sets (16 total))\
-
-#cell1_data = power_on_data[['Cell 1 Current', 'Cell 1 Voltage', 'Cell 1 Temp']]
-#cell2_data = power_on_data[['Cell 2 Current', 'Cell 2 Voltage', 'Cell 2 Temp']]
-#cell3_data = power_on_data[['Cell 3 Current', 'Cell 3 Voltage', 'Cell 3 Temp']]
-#cell4_data = power_on_data[['Cell 4 Current', 'Cell 4 Voltage', 'Cell 4 Temp']] 
-#cell5_data = power_on_data[['Cell 5 Current', 'Cell 5 Voltage', 'Cell 5 Temp']]
-#cell6_data = power_on_data[['Cell 6 Current', 'Cell 6 Voltage', 'Cell 6 Temp']]
-#cell7_data = power_on_data[['Cell 7 Current', 'Cell 7 Voltage', 'Cell 7 Temp']]
-#cell8_data = power_on_data[['Cell 8 Current', 'Cell 8 Voltage', 'Cell 8 Temp']]
-#cell9_data = power_on_data[['Cell 9 Current', 'Cell 9 Voltage', 'Cell 9 Temp']]
-#cell10_data = power_on_data[['Cell 10 Current', 'Cell 10 Voltage', 'Cell 10 Temp']]
-#ell11_data = power_on_data[['Cell 11 Current', 'Cell 11 Voltage', 'Cell 11 Temp']]
-#cell12_data = power_on_data[['Cell 12 Current', 'Cell 12 Voltage', 'Cell 12 Temp']]
-#cell13_data = power_on_data[['Cell 13 Current', 'Cell 13 Voltage', 'Cell 13 Temp']]
-#cell14_data = power_on_data[['Cell 14 Current', 'Cell 14 Voltage', 'Cell 14 Temp']]
-#cell15_data = power_on_data[['Cell 15 Current', 'Cell 15 Voltage', 'Cell 15 Temp']]
-#cell16_data = power_on_data[['Cell 16 Current', 'Cell 16 Voltage', 'Cell 16 Temp']]
 
 
 
-#cell1_data = cell1_data[(cell1_data['Cell 1 Current'] <= .1)]
-#cell2_data = cell2_data[(cell2_data['Cell 2 Current'] <= .1)]
-#cell3_data = cell3_data[(cell3_data['Cell 3 Current'] <= .1)]
-##cell4_data = cell4_data[(cell4_data['Cell 4 Current'] <= .1)]
-#cell5_data = cell5_data[(cell5_data['Cell 5 Current'] <= .1)]
-#cell6_data = cell6_data[(cell6_data['Cell 6 Current'] <= .1)]
-#cell7_data = cell7_data[(cell7_data['Cell 7 Current'] <= .1)]
-#cell8_data = cell8_data[(cell8_data['Cell 8 Current'] <= .1)]
-#cell9_data = cell9_data[(cell9_data['Cell 9 Current'] <= .1)]
-#cell10_data = cell10_data[(cell10_data['Cell 10 Current'] <= .1)]
-#cell11_data = cell11_data[(cell11_data['Cell 11 Current'] <= .1)]
-#cell12_data = cell12_data[(cell12_data['Cell 12 Current'] <= .1)]
-#cell13_data = cell13_data[(cell13_data['Cell 13 Current'] <= .1)]
-#cell14_data = cell14_data[(cell14_data['Cell 14 Current'] <= .1)]
-#cell15_data = cell15_data[(cell15_data['Cell 15 Current'] <= .1)]
-#cell16_data = cell16_data[(cell16_data['Cell 16 Current'] <= .1)]
-
-#analyized_data = pd.concat([cell1_data, cell2_data, cell3_data, cell4_data, cell5_data, cell6_data, cell7_data, cell8_data, cell9_data, cell10_data, cell11_data, cell12_data, cell13_data, cell14_data, cell15_data, cell16_data], axis=1)
-
-#analyized_data.to_csv(f'{experiment_name}analyized_data_{timestamp}.csv', index=False)
 
 
-#Vibe begin
 
 def trim_after_current_drop(df, current_col, timestamp_col, threshold=0.1):
     """Find first row where current <= threshold and keep all rows after, preserving timestamps."""
@@ -318,13 +329,9 @@ def trim_after_current_drop(df, current_col, timestamp_col, threshold=0.1):
 
 cell_dfs = []
 for i in range(1, 17):
-    conc = globals()[f'c{i}conc']
-    curr_col = f"{conc}Cell {i} Current"
-    volt_col = f"{conc}Cell {i} Voltage"
-    if i in [2, 4, 6, 8, 9, 11, 13, 15]:  # Cells with direct temperature sensors
-        temp_col = globals()[f'ts{(i+1)//2}']  # Map cell numbers to ts1-ts8
-    else:
-        temp_col = f"{conc} Cell {i} Temp"
+    curr_col = globals()[f'c{i}current']
+    volt_col = globals()[f'c{i}voltage']
+    temp_col = globals()[f'c{i}temp']
     timestamp_col = "Timestamp"
 
     # Skip cells missing columns
@@ -335,7 +342,7 @@ for i in range(1, 17):
 
     # Include timestamp in the slice
     cell_df = power_on_data[[timestamp_col, curr_col, volt_col, temp_col]].copy()
-    
+
     # Get trimmed data with timestamps
     trimmed_df = trim_after_current_drop(cell_df, curr_col, timestamp_col)
 
