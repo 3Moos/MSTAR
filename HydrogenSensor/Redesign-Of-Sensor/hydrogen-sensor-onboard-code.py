b0840325490559173d7ba15c7cdaf1667b82b13b
3Moos <190341036+3Moos@users.noreply.github.com>
2026-02-02 12:14:55 -0800

    Included Voltage in output + direct Voltage offset for each sensor

--- HydrogenSensor/Redesign-Of-Sensor/hydrogen-sensor-onboard-code.py
+++ HydrogenSensor/Redesign-Of-Sensor/hydrogen-sensor-onboard-code.py
@@ -9,21 +9,42 @@ adc3 = ADC(Pin(28))  # A2
 REF_VOLTAGE = 3.3
 MAX_ADC_VALUE = 65535
 
-V_MIN = 0.02
+V_MIN1 = 0.086824
+V_MIN2 = 0.02
+V_MIN3 = 0.067111
 V_MAX = 3.0
-PPM_MIN = 0
+PPM_MIN1 = 0
+PPM_MIN2 = 0
+PPM_MIN3 = 0
 PPM_MAX = 20000
 
 def adc_to_voltage(raw):
     return (raw / MAX_ADC_VALUE) * REF_VOLTAGE
 
-def calculate_ppm(voltage):
-    if voltage <= V_MIN:
-        return PPM_MIN
+def calculate_ppm1(voltage):
+    if voltage <= V_MIN1:
+        return PPM_MIN1
     elif voltage >= V_MAX:
         return PPM_MAX
     else:
-        return (PPM_MAX - PPM_MIN) * (voltage - V_MIN) / (V_MAX - V_MIN)
+        return (PPM_MAX - PPM_MIN1) * (voltage - V_MIN1) / (V_MAX - V_MIN1)
+
+def calculate_ppm2(voltage):
+    if voltage <= V_MIN2:
+        return PPM_MIN2
+    elif voltage >= V_MAX:
+        return PPM_MAX
+    else:
+        return (PPM_MAX - PPM_MIN2) * (voltage - V_MIN2) / (V_MAX - V_MIN2)
+
+def calculate_ppm3(voltage):
+    if voltage <= V_MIN3:
+        return PPM_MIN3
+    elif voltage >= V_MAX:
+        return PPM_MAX
+    else:
+        return (PPM_MAX - PPM_MIN3) * (voltage - V_MIN3) / (V_MAX - V_MIN3)
+    
 
 while True:
     raw1 = adc1.read_u16()
@@ -34,13 +55,8 @@ while True:
     v2 = adc_to_voltage(raw2)
     v3 = adc_to_voltage(raw3)
 
-    ppm1 = calculate_ppm(v1)
-    ppm2 = calculate_ppm(v2)
-    ppm3 = calculate_ppm(v3)
-
-    print(ppm1, ppm2, ppm3)
-    print(v1, v2, v3)
-    print("          ")
-    sleep(1)
-    print(ppm1, ppm2, ppm3)
+    ppm1 = calculate_ppm1(v1)
+    ppm2 = calculate_ppm2(v2)
+    ppm3 = calculate_ppm3(v3)
+    print(ppm1, v1, " ", ppm2, v2, " ", ppm3, v3)
     sleep(1)
